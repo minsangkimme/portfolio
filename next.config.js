e34585e5bef86af95c5234444b861bcfbdbcbe1a
lorenlee <lorenlee.dev@gmail.com>
2023-08-26 22:01:22 +0900

    modify: set build config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: process.env.NODE_ENV === 'production' ? 'export' : '',
   webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.svg$/,
